71f20c70811aa52cee3cef8dfac4792919305dce
Satoshi Inoue <is5905@paltac.co.jp>
2021-12-29 08:52:03 +0900

    - update: Changed toFlatDict to return {accessor, value} - add usage

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,33 @@
+
+export type AccessorValuePair = {
+  accessor: (string|number)[],
+  value: any
+}
+
 export default class ObjectUtils {
-  static TypeStrings: { [key: string]: TypeString } = {
+
+  // constructor(obj: ({[key: string]: any} | {[key: string]: any}[]), separator: string = '.') {
+  //   const dict = ObjectUtils.toFlatDict(obj, separator);
+
+  //   this._obj = Object.assign({}, dict);
+  // }
+
+  // public getKeys(): string[] {
+  //   return Object.keys(this._obj);
+  // }
+  
+  
+  // public getValue(key: string) : any | undefined {
+  //   return this._obj[key]? this._obj[key].value : undefined;
+  // }
+
+  // public getAccessor(key: string) : (string | number)[] | undefined {
+  //   return this._obj[key]? this._obj[key].accessor: undefined;
+  // }
+
+  // private _obj: {[key: string]: AccessorValuePair};
+
+  public static TypeStrings: { [key: string]: TypeString } = {
     number: '[object Number]',
     string: '[object String]',
     boolean: '[object Boolean]',
@@ -11,7 +39,7 @@ export default class ObjectUtils {
     undefined: '[object Undefined]',
   };
 
-  static primitiveTypes: TypeString[] = [
+  public static primitiveTypes: TypeString[] = [
     '[object Number]',
     '[object String]',
     '[object Boolean]',
@@ -20,68 +48,33 @@ export default class ObjectUtils {
     '[object Undefined]',
   ];
 
-  static objectTypes: TypeString[] = [
+  public static objectTypes: TypeString[] = [
     '[object Array]',
     '[object Object]',
     '[object Function]',
   ];
 
-  static isPrimitive(item: any): boolean {
+  public static isPrimitive(item: any): boolean {
     return ObjectUtils.primitiveTypes.includes(ObjectUtils.getTypeName(item));
   }
 
-  static getTypeName(item: any): TypeString {
+  public static getTypeName(item: any): TypeString {
     return Object.prototype.toString.call(item) as TypeString;
   }
 
-  static hasChildren(obj: any) {
+  public static hasChildren(obj: any) {
     return [
       ObjectUtils.TypeStrings.object,
       ObjectUtils.TypeStrings.array,
     ].includes(ObjectUtils.getTypeName(obj));
   }
 
-  static isMatchType(obj: any, targetTypes?: TypeString[]): boolean {
+  public static isMatchType(obj: any, targetTypes?: TypeString[]): boolean {
     return targetTypes
       ? targetTypes.includes(ObjectUtils.getTypeName(obj))
       : true;
   }
 
-  private static _getPrimitiveKeys(
-    obj: any,
-    keylist: Array<Array<string | number>> = [],
-    parent: Array<string | number> = [],
-  ) {
-    if (ObjectUtils.getTypeName(obj) === ObjectUtils.TypeStrings.object) {
-      for (const key in obj) {
-        const parent_list: Array<string | number> = Array.from(parent);
-        if (Object.hasOwnProperty.call(obj, key)) {
-          parent_list.push(key);
-          const element = obj[key];
-          // if (hasChildren(element)) {
-          if (ObjectUtils.isPrimitive(element)) {
-            keylist.push(parent_list);
-          } else {
-            ObjectUtils._getPrimitiveKeys(element, keylist, parent_list);
-          }
-        }
-      }
-    } else if (ObjectUtils.getTypeName(obj) === ObjectUtils.TypeStrings.array) {
-      for (let index = 0; index < obj.length; index++) {
-        const parent_list: Array<string | number> = Array.from(parent);
-        parent_list.push(index);
-        const element = obj[index];
-        // if (hasChildren(element)) {
-        if (ObjectUtils.isPrimitive(element)) {
-          keylist.push(parent_list);
-        } else {
-          ObjectUtils._getPrimitiveKeys(element, keylist, parent_list);
-        }
-      }
-    }
-
-    return keylist;
-  }
   private static _toFlatDict(
     obj: any,
     separator: string = '.',
@@ -96,7 +89,10 @@ export default class ObjectUtils {
           const element = obj[key];
           // if (hasChildren(element)) {
           if (ObjectUtils.isPrimitive(element)) {
-            dict[parent_list.join(separator)] = element;
+            dict[parent_list.join(separator)] = {
+              accessor: parent_list,
+              value: element,
+            } as AccessorValuePair;
           } else {
             ObjectUtils._toFlatDict(element, separator, dict, parent_list);
           }
@@ -109,7 +105,10 @@ export default class ObjectUtils {
         const element = obj[index];
         // if (hasChildren(element)) {
         if (ObjectUtils.isPrimitive(element)) {
-          dict[parent_list.join(separator)] = element;
+          dict[parent_list.join(separator)] = {
+            accessor: parent_list,
+            value: element,
+          } as AccessorValuePair;
         } else {
           ObjectUtils._toFlatDict(element, separator, dict, parent_list);
         }
@@ -119,21 +118,17 @@ export default class ObjectUtils {
     return dict;
   }
 
-  static toFlatDict(obj: any, separator: string = '.') {
+  public static toFlatDict(obj: any, separator: string = '.'): {[key: string]: AccessorValuePair} {
     return ObjectUtils._toFlatDict(obj, separator);
   }
 
-  static getPrimitiveKeys(obj: any) {
-    return ObjectUtils._getPrimitiveKeys(obj);
-  }
-
-  static getValue(arr: (number | string)[], target: any): any {
-    let v = target;
-    for (let i = 0; i < arr.length; i++) {
-      v = v[arr[i]];
-    }
-    return v;
-  }
+  // static getValue(arr: (number | string)[], target: any): any {
+  //   let v = target;
+  //   for (let i = 0; i < arr.length; i++) {
+  //     v = v[arr[i]];
+  //   }
+  //   return v;
+  // }
 }
 
 export type TypeString =
